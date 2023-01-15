8e3f1f8c3e7ea5bbb1aa7437d7eb71ebc83ba8d5
Ahmed-Shakeel-798 <ahmed.shakeel.work@gmail.com>
2023-01-15 12:49:12 +0500

    memoization

--- memoization/grid__traveller.js
+++ memoization/grid__traveller.js
@@ -5,12 +5,15 @@
  */
 const memo = {};
 const grid_traveller = (m, n) => {
+    console.log("running for: " + m + "," + n);
     const key = m + ',' + n;
+    const rev_key = n + ',' + m; 
     if(key in memo) return memo[key];
     if(m == 1 && n == 1) return 1;
     if(m == 0 || n == 0) return 0;
 
-    memo[key] = grid_traveller(m-1, n) + grid_traveller(m, n-1); 
+    memo[key] = grid_traveller(m-1, n) + grid_traveller(m, n-1);
+    memo[rev_key]  = memo[key]; // x,y produces same result as y,x
     return memo[key];
 }
-console.log(grid_traveller(18,18));
+console.log(grid_traveller(4,3));
